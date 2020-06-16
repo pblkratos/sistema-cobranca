183c6fc1f33d2241f03d371908a965f24691cd26
Pablo <pbl_kratos@hotmail.com>
2020-06-16 20:15:08 -0300

    Adicionado tabela de clientes

--- pages/admin/admins.php
+++ pages/admin/admins.php
@@ -5,26 +5,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Administrador</title>
     </head>
-
     <body>
         <?php
             include_once("navbar.php");
         ?>
         <div class="container-fluid analystics-box">
             <div class="row text-center">
-                <div class="col-lg-4">
-                    <h1 class="analystics analystics-loteamentos">27</h1>
+                <div class="col-lg-4 in-block">
                     <img src="../../images/map2.png" alt="loteamentos cadastrados">
+                    <h1 class="analystics analystics-loteamentos">27</h1>
                     <h6 class="analystics-title">Loteamentos<h6>
                 </div>
                 <div class="col-lg-4">
-                    <h1 class="analystics analystics-clientes">645</h1>
                     <img src="../../images/person2.png" alt="loteamentos cadastrados">
+                    <h1 class="analystics analystics-clientes">645</h1>
                     <h6 class="analystics-title">Clientes<h6>
                 </div>
                 <div class="col-lg-4">
-                    <h1 class="analystics analystics-cobrancas">14</h1>
                     <img src="../../images/money2.png" alt="loteamentos cadastrados">
+                    <h1 class="analystics analystics-cobrancas">14</h1>
                     <h6 class="analystics-title">Cobranças<h6>
                 </div>
             </div>
@@ -38,7 +37,7 @@
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-4">
-                    <a href="client-verify.php">
+                    <a href="clients-verify.php">
                         <div class="menu-item menu-ver-clients"><span>Verificar clientes<span></div>
                     </a>
                 </div>
@@ -50,6 +49,16 @@
                 <div class="col-lg-3 col-md-4">
                     <a href="documents-verify.php">
                     <div class="menu-item menu-documents"><span>Documentos<span></div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <a href="">
+                        <div class="menu-item"><span>Cadastrar Lote<span></div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <a href="">
+                        <div class="menu-item"><span>menu 1<span></div>
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-4">
--- pages/admin/clients-verify.php
+++ pages/admin/clients-verify.php
@@ -9,6 +9,41 @@
         <?php
             include_once("navbar.php");
         ?>
-        verificar clientes
+        <div class="container">
+            <div class="row">
+                <table class="table table-hover table-dark mt-5">
+                    <thead>
+                        <tr>         
+                            <th>Código</th>
+                            <th>Nome Completo</th>
+                            <th>Loteamento</th>
+                            <th>Quadra</th>
+                            <th>Lote</th>
+                            <th>Telefone</th>
+                        </tr>
+                    </thead>
+                        <?php
+                            $selectClient = "SELECT * FROM clientes INNER JOIN lotes WHERE clientes.id=lotes.clientes_id";
+
+                            $dados = mysqli_query($con,$selectClient) or die (mysqli_error());
+                            
+                            $arrClientes = array();
+                            while($linha = mysqli_fetch_array($dados, MYSQLI_ASSOC)){
+                                array_push($arrClientes,$linha);
+                            }
+                            foreach($arrClientes as $value){
+                              echo "<tr>";
+                              echo "<td>". $value["id"]  ."</td>";
+                              echo "<td>". $value["nome"]  ."</td>";
+                              echo "<td>". $value["loteamento"]  ."</td>";
+                              echo "<td>". $value["quadra"]  ."</td>";
+                              echo "<td>". $value["lote"]  ."</td>";
+                              echo "<td>". $value["telefone"]  ."</td>";
+                              echo "</tr>";
+                            }
+                        ?>
+                </table>
+            </div>
+        </div>
     </body>
 </html>
